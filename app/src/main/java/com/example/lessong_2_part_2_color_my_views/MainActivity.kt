e542fa3f860f53ea8f7ca768ae45bd0cff7353da
emdadul_islam <emdadulislam439@gmail.com>
2021-07-25 16:40:41 +0600

    initial commit

---
+++ app/src/main/java/com/example/lessong_2_part_2_color_my_views/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.lessong_2_part_2_color_my_views
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
