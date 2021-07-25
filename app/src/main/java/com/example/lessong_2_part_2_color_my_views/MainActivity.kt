f87e094268228ad2009562d169419b936586e014
emdadul_islam <emdadulislam439@gmail.com>
2021-07-25 16:52:29 +0600

    added data binding

--- app/src/main/java/com/example/lessong_2_part_2_color_my_views/MainActivity.kt
+++ app/src/main/java/com/example/lessong_2_part_2_color_my_views/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.lessong_2_part_2_color_my_views
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.lessong_2_part_2_color_my_views.databinding.ActivityMainBinding
+import com.example.lessong_2_part_2_color_my_views.model.DataClassForBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    lateinit var bindingClass : DataClassForBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        bindingClass = DataClassForBinding("Hello Emdad!")
+
+        binding.bindingClass = bindingClass
     }
 }
---
+++ app/src/main/java/com/example/lessong_2_part_2_color_my_views/model/DataClassForBinding.kt
@@ -0,0 +1,5 @@
+package com.example.lessong_2_part_2_color_my_views.model
+
+data class DataClassForBinding(
+    var text : String = "Hello World!",
+)
